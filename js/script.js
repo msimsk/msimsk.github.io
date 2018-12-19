a44bfa2488b93244c6098fa437d16e76ddaebc10
Mustafa Şimşek <37286220+msimsk@users.noreply.github.com>
2018-12-19 21:10:59 +0300

    script

--- js/script.js
+++ js/script.js
@@ -1,15 +1,11 @@
-function loadSkills(){
-  loadSkill(document.getElementById('Android'),document.getElementById('Android').innerHTML);
-  loadSkill(document.getElementById('MySQL'),document.getElementById('MySQL').innerHTML);
-  loadSkill(document.getElementById('Html'),document.getElementById('Html').innerHTML);
-  loadSkill(document.getElementById('Java'),document.getElementById('Java').innerHTML);
-  loadSkill(document.getElementById('Ruby'),document.getElementById('Ruby').innerHTML);
-  loadSkill(document.getElementById('Python'),document.getElementById('Python').innerHTML);
-  loadSkill(document.getElementById('Eng'),document.getElementById('Eng').innerHTML);
-}
-  function loadSkill(element,percent){
-  setInterval(loader(element,percent),5000);
-}
-  function loader(element,percent){
+loadSkill(document.getElementById('Android'),document.getElementById('Android').innerHTML);
+loadSkill(document.getElementById('MySQL'),document.getElementById('MySQL').innerHTML);
+loadSkill(document.getElementById('Html'),document.getElementById('Html').innerHTML);
+loadSkill(document.getElementById('Java'),document.getElementById('Java').innerHTML);
+loadSkill(document.getElementById('Ruby'),document.getElementById('Ruby').innerHTML);
+loadSkill(document.getElementById('Python'),document.getElementById('Python').innerHTML);
+loadSkill(document.getElementById('Eng'),document.getElementById('Eng').innerHTML);
+
+function loader(element,percent){
   element.style.width = percent;
 }
